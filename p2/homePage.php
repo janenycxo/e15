05fb181104302df28292e9df17fc0849c9e507a6
Jane Yeh <jane.yeh@gmail.com>
2020-03-10 18:48:23 -0400

    logo

--- p2/homePage.php
+++ p2/homePage.php
@@ -16,12 +16,13 @@
     <br>Select "Calculate BMI" and your BMI will appear below.</p>
     <br>
 
-    <img src='/p2/public/images/BMI.jpg' alt="BMI Logo'>
+    <img src='/images/BMI.jpg' alt="BMI Logo'>
+
     <p>BMI Categories:
-<br>Underweight = <18.5
-<br>Normal  = 18.5–24.9
-<br>Overweight = 25–29.9
-<br>Obesity = BMI of 30 or greater
+    <br>Underweight = <18.5
+    <br>Normal  = 18.5–24.9
+    <br>Overweight = 25–29.9
+    <br>Obesity = BMI of 30 or greater
     </p>
    
     <a href='https://www.nhlbi.nih.gov/health/educational/lose_wt/BMI/bmi_tbl.htm'>The BMI Tables</a>
