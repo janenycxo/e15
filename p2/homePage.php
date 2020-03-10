fbe343d1158511efc5ad2f829d070bb1d690ef0e
Jane Yeh <jane.yeh@gmail.com>
2020-03-10 18:13:27 -0400

    P2

--- p2/homePage.php
+++ p2/homePage.php
@@ -21,41 +21,7 @@
 <br>Overweight = 25–29.9
 <br>Obesity = BMI of 30 or greater
     </p>
-    <form method= 'post' action='bmiProcess.php'>
-        <label for='inputString'>Enter a string:</label>
-        <input type='text' id='inputString' name='inputString'>
-        <button type='submit'>Calculate BMI</button>
-    </form>
-     
-     <?php if (isset($results)) :?>
-
-     $_GET['calculate bmi']) { 
-        $weight = $_GET['weight'];
-        $height = $_GET['height'];
-    
-        function bmi($weight,$height) {
-            $bmi = $weight/($height*$height);
-            return $bmi;
-        }   
-    
-        $bmi = bmi($weight,$height); 
-    
-        if ($bmi <= 18.5) {
-            $output = "UNDERWEIGHT";
-    
-            } else if ($bmi > 18.5 AND $bmi<=24.9 ) {
-            $output = "NORMAL";
-    
-            } else if ($bmi > 25 AND $bmi<=29.9) {
-            $output = "OVERWEIGHT";
-    
-            } else if ($bmi > 30.0) {
-            $output = "OBESE";
-        }
-        echo "Your BMI value is  " . $bmi . "  and you are : "; 
-        echo "$output";
-    }
-     
+   
 
 
 
