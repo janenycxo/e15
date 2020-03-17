23a0986c2b73e2e298ee2294a1791dc372c82c0b
Jane Yeh <jane.yeh@gmail.com>
2020-03-17 15:21:29 -0400

    CSS

--- p2/homePage.php
+++ p2/homePage.php
@@ -4,66 +4,133 @@
     <title>e15 Project 2</title>
     <meta charset='utf-8'>
    
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-    <link rel="stylesheet" type="text/css" href="styles.css">
+    <link rel="stylesheet" type="text/css" href="Styling.css">
 </head>
-<body>
-    <br><br>
-   
-    <center><h1>Calculate your Body Mass Index</h1></center>
-    <center><img src="public/images/BMI.jpg"></center>
-   <br><center>Body mass index (BMI) is a measure of body fat based on height and weight that applies to adult men and women.</center>
-    <br><center>Please select your gender, enter your weight and height as indicated below to begin:</center>
-    <br><br>
-    <center>
-    <form method='POST' action='/save-new-result'>
 
-        <br>
-        
-        <p><h2>BMI Calculator</h2></p>
-        <p>Please select your gender:</p>
-              
-          <label for='Male'> Male</label>
-          <input type='radio' value='Gender' id='Male' name='Male'>
-<br>
-          <label for='Female'> Female</label>
-          <input type='radio' value='Gender' id='Female' name='Female>
-          
-             
-         <br><br>
-         
-         Please enter your weight in pounds:
-         <input type='text' id='weight' value='weight' name="Your weight in pounds" />   
-                  
-          <br> <br>
-                 
-         Please enter your height in inches:
-          <input type='text' id='height' value= 'height' name="Your height in inches" />
-          
-          <br><br>
-         
-         Please enter your weight in pounds:
-         <input type='text' id='weight' value='weight' name="Your weight in pounds" />   
-           
-          <br><br>
-          
-          <div>
-    <button class ='submit' type='button' value="Submit" onclick="bmi()" /> Calculate BMI</button>
-          </div>   
-          </form> 
-          </center>
+<body>
+    <header>
+   
+  <h1>
+  
+  Calculate your Body Mass Index</h1>
+  <br>
     
-    <br><br>
-    <center>
     
-    <p>BMI Categories:
+   <img alt="BMI Logo" src="public/images/BMI.jpg">
+    
+    </header>
+    
+   <br>
+   <h2>Body mass index (BMI) is a measure of body fat based on height and weight that applies to adult men and women.</h2>
+   
+   <br>
+    <h3>BMI Categories:</h3>
     <br>Normal  = 18.5–24.9
     <br>Overweight = 25–29.9
     <br>Obesity = 30-39.9
     <br>Morbidly Obese = >40
-    </p>
-   </center>
-        
     
+  
+        
+    <br>
+    <h3>
+    
+    Please follow these guidelines as indicated below to begin: </h3>
+    <br>1. Select your gender.
+    <br>2. Enter your weight.  
+    <br>3. Enter your height.
+  
+    
+    <br><br>
+   <form method='POST' action='/save-new-result'>
+        
+          <br>
+       
+           
+        
+        
+        <br>
+        
+   <fieldset>
+       <legend>
+        <h2>BMI Calculator</h2>
+        </legend>
+      
+        <br>
+       <h3>Please select your Gender:</h3>
+        
+        <div>
+         <label for='Male'> Male</label>
+          <input type='radio' value='Gender' id='Male' name='Male'>
+<br>
+          <label for='Female'> Female</label>
+          <input type='radio' value='Gender' id='Female' name='Female'>
+<br>      
+        </div>
+        
+      <br>
+     <br>    
+           
+          <h3> Please enter your height in Inches:</h3>
+         
+          
+          <div>
+          <input type='text' id='height' placeholder= 'Enter your Height'>
+          </div>
+         
+         <br>
+           <br>                 
+                  
+       
+         <h3>
+         Please enter your weight in pounds:</h3>
+       
+         
+         <input type='text' id='weight' placeholder= 'Enter your Weight'>   
+           
+          <br><br>
+          
+          <div>
+   <div>
+          <br>
+          <button type='submit' title='What is your BMI?'>Calculate BMI</button>
+        </div>
+          </div>   
+        
+          </fieldset>
+            </form> 
+   
+    
+    <br><br>
+    
+ 
+    
+          
+        
+        
+  <h2>Results</h2>
+    
+    <h3>
+    Your BMI is:
+    </h3>
+       
+    <h3>
+    You fall under the range: 
+    </h3>
+      
+      <br><br>
+      
+    
+    <h4>*Note: The BMI calculator is only an estimate and may not be accurate for people with greater muscle mass (such as athletes) or in older people and others who have lost muscle mass. </h4>
+      
+         
+    
+    <footer>
+      <br>
+      <h3>
+      <a href="http://e15p2.janenycxo.me">Click here to recalculate BMI Calculator</a></h3>
+      
+      <br>
+    </footer>  
 </body>
 </html>
