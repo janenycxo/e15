d30b9d0b32e32f44c425cd75d7368f7eb6af2529
Jane Yeh <jane.yeh@gmail.com>
2020-03-17 20:54:38 -0400

    Clong P2

---
+++ p2a/resources/views/layouts/master.blade.php
@@ -0,0 +1,36 @@
+<!doctype html>
+<html lang='en'>
+
+<head>
+    <title>@yield('title', 'E-15 Project 2 BMI Calculator')</title>
+    <meta charset='utf-8'>
+
+    <link href='/css/styling.css' rel='stylesheet'>
+
+    @yield('head')
+</head>
+
+<body>
+
+    <header>
+    <a href='/'><img src='public/images/BMI.jpg' id='logo' alt='BMI Logo'></a>
+        <h1>E-15 Project 2 BMI Calculator</h1>
+
+    </header>
+
+    <section>
+        @yield('content')
+    </section>
+
+    <footer>
+      <br>
+      <h3>
+      <a href="http://e15p2.janenycxo.me">Click here to recalculate BMI Calculator</a></h3>
+      
+      <br>
+    </footer>  
+
+    <br>
+</body>
+
+</html>
---
+++ p2a/routes/web.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+# Homepage
+Route::get('/', 'homePage@index');
+
+# Forms
+Route::get('/forms', 'BmiFormController@index');
+Route::get('/forms/{title?}', 'BmiFormController@show');
