01bc71b2c72827e7beb409053518efeb87b6d15a
Jane Yeh <jane.yeh@gmail.com>
2020-04-02 00:35:55 -0400

    Bookmark

--- bookmark/routes/web.php
+++ bookmark/routes/web.php
@@ -10,17 +10,72 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::any('/practice/{n?}', 'PracticeController@index');
 
-Route::get('/', function () {
+//practice/1
+
+//Route::get('/', function () {
     //return'<h1>Jane says hello!</h1>';
-    return view('welcome');
-});
+  //  return view('welcome');
+//});
 
 //Additional routes
 Route::get('/example', function () {
-    return 'Example...';
+   // return 'Example...';
+   return view('abc');
 });
 
-Route::get('/book/{title}', function () {
-    return 'Testing...';
+//Route::get('/book/{title}', function () {
+  //  return 'Testing...';
+//});
+
+Route::get('/debug', function () {
+
+    $debug = [
+        'Environment' => App::environment(),
+  ];
+
+    /*
+    The following commented out line will print your MySQL credentials.
+    Uncomment this line only if you're facing difficulties connecting to the
+    database and you need to confirm your credentials. When you're done
+    debugging, comment it back out so you don't accidentally leave it
+    running on your production server, making your credentials public.
+    */
+    #$debug['MySQL connection config'] = config('database.connections.mysql');
+
+  try {
+        $databases = DB::select('SHOW DATABASES;');
+        $debug['Database connection test'] = 'PASSED';
+        $debug['Databases'] = array_column($databases, 'Database');
+    } catch (Exception $e) {
+        $debug['Database connection test'] = 'FAILED: '.$e->getMessage();
+    }
+
+    dump($debug);
 });
+
+
+/**
+ * Miscellaneous mostly-static pages
+ */
+Route::get('/', 'PageController@welcome');
+Route::get('/support', 'PageController@support');
+
+
+/**
+ * Books
+ */
+Route::get('/books/create', 'BookController@create');
+Route::post('/books', 'BookController@store');
+
+Route::get('/books', 'BookController@index');
+Route::get('/books/{slug?}', 'BookController@show');
+
+Route::get('/search', 'BookController@search');
+
+Route::get('/list', 'BookController@list');
+
+# This was an example route to show multiple parameters;
+# Not a feature we're actually building, so I'm commenting out
+# Route::get('/filter/{category}/{subcategory?}', 'BookController@filter');
