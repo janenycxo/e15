400ead628f6a327d766525984395025c760b5dc9
Jane Yeh <jane.yeh@gmail.com>
2020-05-01 05:12:03 -0400

    testing routes

--- p3/resources/views/pages/welcome.blade.php
+++ p3/resources/views/pages/welcome.blade.php
@@ -2,21 +2,20 @@
 <html lang='en'>
 
 <head>
-    <title>@yield('title', 'Project 3 Jando Eats')</title>
+    <title>'Project 3 Jando Eats'</title>
     <meta charset='utf-8'>
  
     <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <link href='/css/bookmark.css' rel='stylesheet'>
 
-    @yield('head')
 </head>
 <body>
 @if(session('flash-alert'))
     <div class='flash-alert'>
         {{ session('flash-alert') }}
     </div>
-    @endif
+@endif
 
 <header>
       <img src='/JandoEatsKnifeandfork.jpg'>
@@ -30,23 +29,23 @@
             <ul>
                 <li><a href='/'>Home</a></li>
 
-                @if(Auth::user())
+@if(Auth::user())
                 <li><a href='/all'>All Restaurants</a></li>
                 <li><a href='/restaurants/create'>Add a Restaurant</a></li>
                 <li><a href='/list'>Your Saved Restaurant List</a></li>
-                @endif
+@endif
 
                 <li><a href='/support'>Support</a></li>
 
                 <li>
-                    @if(!Auth::user())
+@if(!Auth::user())
                         <a href='/login'>Login</a>
-                    @else
+@else
                         <form method='POST' id='logout' action='/logout'>
                             {{ csrf_field() }}
                             <a href='#' onClick='document.getElementById("logout").submit();'>Logout {{ $user->name }}</a>
                         </form>
-                    @endif
+@endif
                 </li>
             </ul>
         </nav>
@@ -56,7 +55,7 @@
 <br>
 
     <section id='main'>
-        @yield('content')
+@yield('content')
     </section>
 
     <footer>
