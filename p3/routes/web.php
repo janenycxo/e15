0a87d7e209d0b08b4d97d548dcc2c48b981599d0
Jane Yeh <jane.yeh@gmail.com>
2020-05-01 05:15:18 -0400

    testing routes

--- p3/routes/web.php
+++ p3/routes/web.php
@@ -40,59 +40,60 @@ use Illuminate\Support\Facades\Route;
 //}); 
 
     Route::get('/', function () {
-        return view('welcome');
+        return view 'testing routes';
     });
+
 
 /**
  * Miscellaneous mostly-static pages
  */
-    Route::get('/', 'PageController@welcome');
-    Route::get('/support', 'PageController@support');
+    //Route::get('/', 'PageController@welcome');
+    //Route::get('/support', 'PageController@support');
 
 
 /**
  * Restaurants
  */
-    Route::group(['middleware' => 'auth'], function () {
+    //Route::group(['middleware' => 'auth'], function () {
     # Add a restaurant
-    Route::get('/restaurants/create', 'RestaurantController@create');
-    Route::post('/restaurants', 'RestaurantController@store');
+    //Route::get('/restaurants/create', 'RestaurantController@create');
+    //Route::post('/restaurants', 'RestaurantController@store');
 
     # Update a restaurant
-    Route::get('/restaurants/{slug}/edit', 'RestaurantController@edit');
-    Route::put('/restaurants/{slug}', 'RestaurantController@update');
+    //Route::get('/restaurants/{slug}/edit', 'RestaurantController@edit');
+    //Route::put('/restaurants/{slug}', 'RestaurantController@update');
 
     # Show list of restaurants
-    Route::get('/restaurants', 'RestaurantController@index');
+    //Route::get('/restaurants', 'RestaurantController@index');
 
     # Search for a restaurant
-    Route::get('/search}', 'RestaurantController@search');
+    //Route::get('/search}', 'RestaurantController@search');//
 
     # Delete
-    Route::get('/restaurant/{slug}/delete', 'RestaurantController@delete');
+    //Route::get('/restaurant/{slug}/delete', 'RestaurantController@delete');
 
     # Process deletion of restaurant
-    Route::delete('/restaurant/{slug}', 'RestaurantController@destroy');
-    });
+    //Route::delete('/restaurant/{slug}', 'RestaurantController@destroy');
+    //});
 
     
     /**
      * List routes
      */
     # Show your list
-    Route::get('/list', 'ListController@show');
+    //Route::get('/list', 'ListController@show');
 
     # Page to add a restaurant to your list
-    Route::get('/list/{slug?}/add', 'ListController@add');
+    //Route::get('/list/{slug?}/add', 'ListController@add');
 
     # Process adding a restaurant to your list
-    Route::post('/list/{slug?}/add', 'ListController@save');
+    //Route::post('/list/{slug?}/add', 'ListController@save');
 
 
     # Misc
-    Route::get('/search', 'RestaurantController@search');
-    Route::get('/list', 'RestaurantController@list');
+    ///Route::get('/search', 'RestaurantController@search');
+    //Route::get('/list', 'RestaurantController@list');
 
 
 
-    Auth::routes();
+    //Auth::routes();
