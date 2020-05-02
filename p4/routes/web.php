e22571969ee1c872e9544c8eff164b6a4559f71e
Jane Yeh <jane.yeh@gmail.com>
2020-05-02 15:58:11 -0400

    update routes

--- p4/routes/web.php
+++ p4/routes/web.php
@@ -23,11 +23,3 @@ Route::get('/', function () {
     return view('welcome');
 });
 Auth::routes();
-
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
-
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
